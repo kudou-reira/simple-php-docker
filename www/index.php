45b8ed04c78f86fe87f5df71f7b662ad5a307ba7
kudou reira <h.i.y.e.o.XD@gmail.com>
2018-04-18 00:40:29 +0900

    updated stuff

--- www/index.php
+++ www/index.php
@@ -1,4 +1,48 @@
 <?php
-   echo '<script>console.log("Your stuff here")</script>';
+	require_once('./db/db.php');
+	// require_once('./api/api.php');
+
+	// $day = 'Thursday';
+	// $date = 31;
+
+	// echo 'The date is '.$day.' '.$date;
+	// $text = 'Hello';
+	// $text .= ' World';
+
+	// echo("this is server request method".$_SERVER['REQUEST_METHOD'])
+
+
+	$request_body = file_get_contents('php://input');
+	print($request_body);
+
+	if($_SERVER['REQUEST_METHOD'] == "GET") {
+		echo "Hello World";
+	} else if($_SERVER['REQUEST_METHOD'] == "POST") {
+		echo "POST";
+	} else {
+		http_response_code(405);
+	}
+
+	// print("hello")
+
+	// class APIResult
+	// {
+	//     public $status = "ok";
+	//     public $data;
+	//     public $jwt;
+	//     public $jwt_error = null;
+	// }
+
+	// $test = new APIResult;
+	// $test->data = "this is data";
+
+	// print_r($test)
 ?>
 
+<!-- {
+	"url":"/api",
+	"action": "data",
+	"parameters": {
+		"phone": 8109024516691
+	}
+} -->
